0454ee18b3059a7415e13ff00ab384220a4e71a5
Victor <ninjavigge@gmail.com>
2023-12-19 16:44:44 +0100

    spara

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
 
+function nota()
+    SET variabel nota = 0;
+    SET variabel antalVänner = 0;
+    set variabel dricks = 0;
+
+    if(nota > 0 && antalVänner > 0)
+        om notan && antalVänner är större 0 ska de två delas på varandra
+        nota % antalVänner
+    end if
+
+    if(dricks > 0)
+        om användaren skriver in en summen mer än 0 ska då dricken printas ut
+        PRINT "dricks"
+    end if
+
+end function
 
 */
 
@@ -10,16 +26,21 @@
 /*
 
 function play()
-    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
+    SET variabel ordBok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
     SET variabel startOrd till "FOUR";
-    SET variabel slutordOrd till "FIVE";
+    SET variabel slutOrd till "FIVE";
+    SET variabel ord till användarinput;
+
+    if(ordet som skrivs in inte innehåller ordBok)
+        PRINT "Ordet finns inte i det engelska språket"
+    end if
 
 
 end function
 
 function isOneLetterApart(wordOne, wordTwo)
     SET variabel diffCount till 0;
-
+        
 
     return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
 end function
